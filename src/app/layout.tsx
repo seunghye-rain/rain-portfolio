fa61e27364e8045957f0cef8f02a3aac53626d3d
bhyerain <seunghye0307@naver.com>
2026-08-05 00:34:05 +0900

    feat: 메타데이터 추가 및 뷰포트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
 import '@/styles/globals.css';
 
+import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
 });
+
+export const metadata: Metadata = {
+  title: '양승혜 포트폴리오',
+  description: 'Rain Portfolio',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  } as Viewport,
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
