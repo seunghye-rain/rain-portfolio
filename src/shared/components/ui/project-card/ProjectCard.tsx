66afe0ad14c6451df824c7500bee87e556eab211
bhyerain <seunghye0307@naver.com>
2026-08-06 00:52:42 +0900

    feat: 프로젝트 카드 컴포넌트에 이미지 비율 상수로 설정

--- src/shared/components/ui/project-card/ProjectCard.tsx
+++ src/shared/components/ui/project-card/ProjectCard.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
-
-const IMAGE_RATIO_CLASS: Record<'default' | 'portrait', string> = {
-  default: 'aspect-[300/169]',
-  portrait: 'aspect-[4/5]',
-};
+import type { ImageRatio } from '@/shared/constants/imageRatio';
+import { IMAGE_RATIO_CLASS } from '@/shared/constants/imageRatio';
 
 type ProjectCardProps = {
   title: string;
   description: string;
   imageSrc: string;
   imageAlt: string;
-  imageRatio?: 'default' | 'portrait';
+  href: string;
+  imageRatio?: ImageRatio;
 };
 
 export const ProjectCard = ({
@@ -20,22 +18,26 @@ export const ProjectCard = ({
   description,
   imageSrc,
   imageAlt,
+  href,
   imageRatio = 'default',
 }: ProjectCardProps) => {
   return (
-    <div className='border-black-4 bg-black-1 flex w-[30rem] flex-col items-start gap-[1rem] rounded-[1.6rem] border pb-[1.2rem]'>
+    <a
+      href={href}
+      className='border-black-4 bg-black-1 flex w-[30rem] flex-col items-start gap-[1rem] rounded-[1.6rem] border pb-[1.2rem]'
+    >
       <div
         className={cn(
           'relative w-full overflow-hidden rounded-t-[1.6rem]',
           IMAGE_RATIO_CLASS[imageRatio],
         )}
       >
-        <Image src={imageSrc} alt={imageAlt} fill className='object-cover' />
+        <Image src={imageSrc} alt={imageAlt} fill className='object-cover object-center' />
       </div>
       <div className='flex w-full flex-col items-start gap-[1rem] px-[1.6rem]'>
-        <p className='font-18-bd text-black-10 whitespace-nowrap'>{title}</p>
-        <p className='font-16-md text-black-7'>{description}</p>
+        <p className='font-18-sb text-black-10 whitespace-nowrap'>{title}</p>
+        <p className='font-16-rg text-black-7'>{description}</p>
       </div>
-    </div>
+    </a>
   );
 };
