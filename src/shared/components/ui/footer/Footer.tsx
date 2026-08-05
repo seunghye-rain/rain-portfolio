e98c877cf270da384b67a5bb5f8f57e7c1d072ac
bhyerain <seunghye0307@naver.com>
2026-08-05 14:54:22 +0900

    feat: 푸터 컴포넌트 구현

---
+++ src/shared/components/ui/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { IconLetter, IconPhone } from '@/shared/assets';
+
+export const Footer = () => {
+  return (
+    <footer className='border-black-4 bg-black-2 flex w-full items-center justify-between border-t px-[8rem] py-[3rem]'>
+      <div className='px-[3.6rem] py-[1.8rem]'>
+        <p className='font-16-rg text-black-6'>Copyright © 2026 양승혜</p>
+      </div>
+      <div className='flex flex-col items-start gap-[1rem] px-[1.8rem]'>
+        <div className='flex items-center gap-[0.8rem]'>
+          <IconLetter className='size-[2.4rem]' />
+          <span className='font-18-md text-black-8'>seunghye0307@naver.com</span>
+        </div>
+        <div className='flex items-center gap-[0.8rem]'>
+          <IconPhone className='size-[2.4rem]' />
+          <span className='font-18-md text-black-8'>010-7137-2307</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,2 +1,3 @@
 // ⚠️ 자동 생성된 파일입니다. 직접 수정하지 마세요.
+export * from './footer/index';
 export * from './header/index';
