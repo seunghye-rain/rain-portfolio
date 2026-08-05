91e785e4aa35e17befc2e0baa76c3b06e96231bf
bhyerain <seunghye0307@naver.com>
2026-08-05 17:05:37 +0900

    feat: 섹션 제목 컴포넌트 구현

---
+++ src/shared/components/ui/section-title/SectionTitle.tsx
@@ -0,0 +1,16 @@
+type SectionTitleProps = {
+  title: string;
+  showIcon?: boolean;
+};
+
+export const SectionTitle = ({ title, showIcon = true }: SectionTitleProps) => {
+  return (
+    <div className='flex items-center'>
+      {showIcon && <p className='display-sb-42 text-black-10 px-[2.9rem] py-[2.2rem]'>🌧️</p>}
+      <div className='flex flex-col items-start pr-[2.9rem]'>
+        <p className='display-sb-36 text-black-10 whitespace-nowrap'>{title}</p>
+        <div className='bg-yellow-1/30 mb-[-1.2rem] h-[1.2rem] w-full' />
+      </div>
+    </div>
+  );
+};
