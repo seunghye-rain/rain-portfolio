305996f6af2165fad2282f304d6c8d2544bdf6f0
bhyerain <seunghye0307@naver.com>
2026-08-05 14:25:03 +0900

    style: 헤더 디자인 수정

--- src/shared/components/ui/header/Header.tsx
+++ src/shared/components/ui/header/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <header className='bg-black-1 flex w-full items-center justify-center gap-[12.6rem] py-[1.8rem]'>
       <Image src='/imgs/logo.png' alt='양승혜 포트폴리오' width={263} height={62} />
-      <nav className='stretch flex items-start gap-[4rem]'>
+      <nav className='flex items-stretch gap-[4rem]'>
         {NAV_ITEMS.map((item) => (
           <HeaderNavLink key={item.href} href={item.href} label={item.label} />
         ))}
