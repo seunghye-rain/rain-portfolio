3acfd66e2781d0e6eb9d43598588ffb71c2d8685
bhyerain <seunghye0307@naver.com>
2026-08-05 23:23:59 +0900

    feat: 섹션 제목 컴포넌트에 아이콘 속성 추가

--- src/shared/components/ui/section-title/SectionTitle.tsx
+++ src/shared/components/ui/section-title/SectionTitle.tsx
@@ -1,12 +1,26 @@
+import type { ComponentType } from 'react';
+
+import type { IconProps } from '@/shared/assets';
+import { IconRain } from '@/shared/assets';
+
 type SectionTitleProps = {
   title: string;
   showIcon?: boolean;
+  icon?: ComponentType<IconProps>;
 };
 
-export const SectionTitle = ({ title, showIcon = true }: SectionTitleProps) => {
+export const SectionTitle = ({
+  title,
+  showIcon = true,
+  icon: Icon = IconRain,
+}: SectionTitleProps) => {
   return (
     <div className='flex items-center'>
-      {showIcon && <p className='display-sb-42 text-black-10 px-[2.9rem] py-[2.2rem]'>🌧️</p>}
+      {showIcon && (
+        <div className='px-[2.9rem] py-[2.2rem]'>
+          <Icon className='size-[4.2rem]' />
+        </div>
+      )}
       <div className='flex flex-col items-start pr-[2.9rem]'>
         <p className='display-sb-36 text-black-10 whitespace-nowrap'>{title}</p>
         <div className='bg-yellow-1/30 mt-[-1.2rem] h-[1.2rem] w-full' />
