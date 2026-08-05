5bbd2da3f829d85a7c82ea1a83131d0102c40334
bhyerain <seunghye0307@naver.com>
2026-08-05 23:11:45 +0900

    style: 섹션 제목 컴포넌트의 여백 수정

--- src/shared/components/ui/section-title/SectionTitle.tsx
+++ src/shared/components/ui/section-title/SectionTitle.tsx
@@ -9,7 +9,7 @@ export const SectionTitle = ({ title, showIcon = true }: SectionTitleProps) => {
       {showIcon && <p className='display-sb-42 text-black-10 px-[2.9rem] py-[2.2rem]'>🌧️</p>}
       <div className='flex flex-col items-start pr-[2.9rem]'>
         <p className='display-sb-36 text-black-10 whitespace-nowrap'>{title}</p>
-        <div className='bg-yellow-1/30 mb-[-1.2rem] h-[1.2rem] w-full' />
+        <div className='bg-yellow-1/30 mt-[-1.2rem] h-[1.2rem] w-full' />
       </div>
     </div>
   );
