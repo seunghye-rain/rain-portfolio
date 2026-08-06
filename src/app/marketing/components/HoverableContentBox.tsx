e6bb5073bec32340ce9a50fb18deb8b814014f79
bhyerain <seunghye0307@naver.com>
2026-08-06 17:24:45 +0900

    feat: HoverableContentBox 컴포넌트 추가

---
+++ src/app/marketing/components/HoverableContentBox.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import { IconCircledright } from '@/shared/assets';
+import { ContentWriteBox } from '@/shared/components/ui';
+
+type HoverableContentBoxProps = {
+  href: string;
+  title: string;
+  description: string[];
+};
+
+export const HoverableContentBox = ({ href, title, description }: HoverableContentBoxProps) => {
+  return (
+    <Link href={href} className='group relative block self-start overflow-hidden rounded-[1.6rem]'>
+      <ContentWriteBox title={title} description={description} />
+      <div className='bg-black-7/60 absolute inset-0 flex items-center justify-center gap-[2rem] opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
+        <span className='display-sb-42 text-black-10'>더 알아보기</span>
+        <span className='bg-black-1 flex size-[5rem] shrink-0 items-center justify-center rounded-full'>
+          <IconCircledright className='size-[7.2rem] -rotate-45' />
+        </span>
+      </div>
+    </Link>
+  );
+};
