2aac6b35e10a3515cfe6ce0772151d79a24c3e3c
bhyerain <seunghye0307@naver.com>
2026-08-07 02:03:53 +0900

    feat: 프로젝트 섹션 및 기술 섹션 콘텐츠 추가

---
+++ src/app/develop/mocks/skills.ts
@@ -0,0 +1,16 @@
+// 무한 스크롤 SKILL 섹션 콘텐츠
+
+export type DevelopSkill = {
+  label: string;
+  src: string;
+};
+
+export const DEVELOP_SKILLS: DevelopSkill[] = [
+  { label: 'Typescript', src: '/imgs/skill-content/img-typescript.png' },
+  { label: 'JavaScript', src: '/imgs/skill-content/img-js.png' },
+  { label: 'React', src: '/imgs/skill-content/img-react.png' },
+  { label: 'Next.js', src: '/imgs/skill-content/img-nextjs.png' },
+  { label: 'React Native', src: '/imgs/skill-content/img-react.png' },
+  { label: 'Vanilla-Extract', src: '/imgs/skill-content/img-vanilaextract.png' },
+  { label: 'Tailwind', src: '/imgs/skill-content/img-tailwind.png' },
+];
