e126ea7cfe3b8bf78aad1c23f4ceb8122a95943d
bhyerain <seunghye0307@naver.com>
2026-08-06 19:04:45 +0900

    feat: ProjectCard 컴포넌트의 이미지 속성을 선택적으로 변경 및 스타일 수정

--- src/shared/components/ui/project-card/ProjectCard.tsx
+++ src/shared/components/ui/project-card/ProjectCard.tsx
@@ -7,8 +7,8 @@ import { IMAGE_RATIO_CLASS } from '@/shared/constants/imageRatio';
 type ProjectCardProps = {
   title: string;
   description: string;
-  imageSrc: string;
-  imageAlt: string;
+  imageSrc?: string;
+  imageAlt?: string;
   href: string;
   imageRatio?: ImageRatio;
 };
@@ -28,11 +28,13 @@ export const ProjectCard = ({
     >
       <div
         className={cn(
-          'relative w-full overflow-hidden rounded-t-[1.6rem]',
+          'bg-black-4 relative w-full overflow-hidden rounded-t-[1.6rem]',
           IMAGE_RATIO_CLASS[imageRatio],
         )}
       >
-        <Image src={imageSrc} alt={imageAlt} fill className='object-cover object-center' />
+        {imageSrc && (
+          <Image src={imageSrc} alt={imageAlt ?? ''} fill className='object-cover object-center' />
+        )}
       </div>
       <div className='flex w-full flex-col items-start gap-[1rem] px-[1.6rem]'>
         <p className='font-18-sb text-black-10 line-clamp-2'>{title}</p>
