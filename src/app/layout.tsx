d237fc292c676a25ccdc09236a043d4770d0e085
bhyerain <seunghye0307@naver.com>
2026-08-06 14:07:03 +0900

    feat: body에 패딩 추가하여 레이아웃 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko' className={pretendard.className}>
-      <body>
+      <body className='pt-[9.8rem]'>
         <Header />
         <main className='px-[8rem] py-[8rem]'>{children}</main>
         <Footer />
