b23cbbae18007f23bdc52394dbb2813697b97ab7
bhyerain <seunghye0307@naver.com>
2026-08-06 14:07:27 +0900

    feat: 헤더 스타일 개선 및 내비게이션 링크의 전환 효과 추가

--- src/shared/components/ui/header/Header.tsx
+++ src/shared/components/ui/header/Header.tsx
@@ -6,9 +6,9 @@ import { HeaderNavLink } from './HeaderNavLink';
 
 export const Header = () => {
   return (
-    <header className='bg-black-1 flex w-full items-center justify-center gap-[12.6rem] py-[1.8rem]'>
+    <header className='bg-black-1 border-black-5 fixed inset-x-0 top-0 z-50 flex h-[9.8rem] w-full items-center gap-[12.6rem] border-b pl-[8rem]'>
       <Image src='/imgs/logo.png' alt='양승혜 포트폴리오' width={263} height={62} />
-      <nav className='flex items-stretch gap-[4rem]'>
+      <nav className='flex items-stretch gap-[3rem]'>
         {NAV_ITEMS.map((item) => (
           <HeaderNavLink key={item.href} href={item.href} label={item.label} />
         ))}
