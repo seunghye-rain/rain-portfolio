b29df019ac691fa0800b64822a787fb3b0c8e13d
bhyerain <seunghye0307@naver.com>
2026-08-06 17:24:37 +0900

    feat: ContentWriteBox 컴포넌트에서 description을 배열로 처리하도록 수정

--- src/shared/components/ui/content-write-box/ContentWriteBox.tsx
+++ src/shared/components/ui/content-write-box/ContentWriteBox.tsx
@@ -1,13 +1,19 @@
 type ContentWriteBoxProps = {
   title: string;
-  description: string;
+  description: string | string[];
 };
 
 export const ContentWriteBox = ({ title, description }: ContentWriteBoxProps) => {
+  const paragraphs = Array.isArray(description) ? description : [description];
+
   return (
     <div className='border-yellow-1 bg-yellow-5/60 flex flex-col items-center justify-center gap-[2.5rem] rounded-[1.6rem] border-[1.5px] px-[5rem] py-[4rem]'>
       <span className='title-30-eb text-black-10 text-center'>{title}</span>
-      <p className='title-20-rg text-black-9'>{description}</p>
+      <div className='title-20-rg text-black-9 flex w-[40rem] flex-col gap-[1.2rem]'>
+        {paragraphs.map((paragraph) => (
+          <p key={paragraph}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   );
 };
