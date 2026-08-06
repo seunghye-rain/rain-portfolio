6f83c20963c18309dc2624ef0d42a1f80a4747f7
bhyerain <seunghye0307@naver.com>
2026-08-07 02:04:04 +0900

    feat: ProjectTabs 컴포넌트에 스크롤 애니메이션 추가 및 코드 정리

--- src/app/marketing/contents/components/ProjectTabs.tsx
+++ src/app/marketing/contents/components/ProjectTabs.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
 import { IconSmartphone } from '@/shared/assets';
 import { SectionTitle, Tabs } from '@/shared/components/ui';
 
@@ -12,13 +16,35 @@ type ProjectTabsProps = {
 };
 
 export const ProjectTabs = ({ activeTab }: ProjectTabsProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const prevTabRef = useRef(activeTab);
+
+  useEffect(() => {
+    const hasChanged = prevTabRef.current !== activeTab;
+    prevTabRef.current = activeTab;
+    if (!hasChanged) return;
+
+    const bottom = ref.current?.getBoundingClientRect().bottom;
+    if (bottom === undefined) return;
+    window.scrollTo({ top: window.scrollY + bottom, behavior: 'smooth' });
+  }, [activeTab]);
+
   return (
-    <div className='bg-black-1 sticky top-[9.8rem] z-40 -mx-[8rem] flex w-[calc(100%+16rem)] flex-col items-center px-[8rem]'>
+    <div
+      ref={ref}
+      className='bg-black-1 sticky top-[9.8rem] z-40 -mx-[8rem] flex w-[calc(100%+16rem)] flex-col items-center px-[8rem]'
+    >
       <SectionTitle id='marketing-contents-project-title' title='Project' icon={IconSmartphone} />
       <Tabs>
         <Tabs.List className='justify-center'>
           {TAB_ITEMS.map((tab) => (
-            <Tabs.Item key={tab.value} value={tab.value} activeValue={activeTab} href={tab.href}>
+            <Tabs.Item
+              key={tab.value}
+              value={tab.value}
+              activeValue={activeTab}
+              href={tab.href}
+              size='md'
+            >
               {tab.label}
             </Tabs.Item>
           ))}
