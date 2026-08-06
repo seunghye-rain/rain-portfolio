923a2ba05ba6326bfa13b7a99abc92c72e3b7549
bhyerain <seunghye0307@naver.com>
2026-08-06 17:01:06 +0900

    feat: 마케팅 페이지의 세부 페이지 라우터 설정

--- src/app/marketing/contents/page.tsx
+++ src/app/marketing/contents/page.tsx
@@ -1,7 +1,7 @@
-export default function PlanningPage() {
+export default function ContentsPage() {
   return (
     <main className='flex min-h-screen items-center justify-center'>
-      <h1 className='text-3xl font-bold'>Planning</h1>
+      <h1 className='text-3xl font-bold'>Contents</h1>
     </main>
   );
 }
