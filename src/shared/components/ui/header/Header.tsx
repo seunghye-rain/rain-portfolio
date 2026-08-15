67a88c9b66d235a3fa67d6f627d5898cd9e64cea
bhyerain <seunghye0307@naver.com>
2026-08-15 23:42:19 +0900

    refactor: 헤더 컴포넌트 스타일 수정 및 반응형 디자인 개선

--- src/shared/components/ui/header/Header.tsx
+++ src/shared/components/ui/header/Header.tsx
@@ -3,20 +3,28 @@ import Link from 'next/link';
 
 import { NAV_ITEMS } from '@/shared/constants/navItems';
 
+import { HeaderMobileMenu } from './HeaderMobileMenu';
 import { HeaderNavLink } from './HeaderNavLink';
 
 export const Header = () => {
   return (
-    <header className='bg-black-1 border-black-5 fixed inset-x-0 top-0 z-50 grid h-[9.8rem] w-full grid-cols-[minmax(26.3rem,1fr)_max-content_minmax(0,1fr)] items-center border-b px-[8rem]'>
-      <Link href='/' aria-label='홈으로 이동' className='justify-self-start'>
-        <Image src='/imgs/logo.png' alt='양승혜 포트폴리오' width={263} height={62} />
+    <header className='bg-black-1 border-black-5 fixed inset-x-0 top-0 z-50 flex h-[6.4rem] w-full items-center justify-between border-b px-[2rem] lg:grid lg:h-[9.8rem] lg:grid-cols-[minmax(26.3rem,1fr)_max-content_minmax(0,1fr)] lg:px-[8rem]'>
+      <Link href='/' aria-label='홈으로 이동' className='lg:justify-self-start'>
+        <Image
+          src='/imgs/logo.png'
+          alt='양승혜 포트폴리오'
+          width={263}
+          height={62}
+          className='h-auto w-[14rem] lg:w-[26.3rem]'
+        />
       </Link>
-      <nav className='flex items-stretch gap-[3rem] justify-self-center'>
+      <nav className='hidden items-stretch gap-[3rem] lg:flex lg:justify-self-center'>
         {NAV_ITEMS.map((item) => (
           <HeaderNavLink key={item.href} href={item.href} label={item.label} />
         ))}
       </nav>
-      <div aria-hidden />
+      <div aria-hidden className='hidden lg:block' />
+      <HeaderMobileMenu />
     </header>
   );
 };
